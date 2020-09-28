a4fd4e30a66f084988555da953c0e5aa57232987
priyanshi <priyansnshi123@gmail.com>
2020-09-29 02:56:03 +0530

    Updates

---
+++ precache-manifest.b5b5d3607c7477d8bc7b34dcaabce07c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0c1a3bd87e017e58b43e6cdfdbd9035",
+    "url": "/proejctproduct/index.html"
+  },
+  {
+    "revision": "9f6c6ece83b120a73dca",
+    "url": "/proejctproduct/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "6fd937a7dc4e3b379f46",
+    "url": "/proejctproduct/static/css/main.c87ed86d.chunk.css"
+  },
+  {
+    "revision": "9f6c6ece83b120a73dca",
+    "url": "/proejctproduct/static/js/2.74408029.chunk.js"
+  },
+  {
+    "revision": "7bfaa32b315fe466d84ab2ae49052d17",
+    "url": "/proejctproduct/static/js/2.74408029.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fd937a7dc4e3b379f46",
+    "url": "/proejctproduct/static/js/main.c73f5c0b.chunk.js"
+  },
+  {
+    "revision": "3c84f0931f1ee98d31bf",
+    "url": "/proejctproduct/static/js/runtime-main.176635fc.js"
+  }
+]);
